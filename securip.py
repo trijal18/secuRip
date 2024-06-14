68c0498e4cdb2b63efe708bb5d1d4532bb05cbf6
Trijal <123671129+trijal18@users.noreply.github.com>
2024-06-14 17:03:57 +0530

    Update securip.py

--- securip.py
+++ securip.py
@@ -4,46 +4,38 @@ import string
 import pandas as pd
 import selenium
 
-names=pd.read_csv(r"D:\projects\secuRIP\Indian-Male-Names.csv") #path to dataset
+names=pd.read_csv(r"secuRIP\Indian-Male-Names.csv") #path to dataset
 
-def get_name():
-     
-    random_index = random.choice(names.index)
-        
-        # Retrieve the name at the randomly chosen index
+#get name form dataset
+def get_name(): 
+    random_index = random.choice(names.index)    
+    # Retrieve the name at the randomly chosen index
     random_name = names.loc[random_index, 'Name']
         
     return str(random_name)
 
+#genrate fake email coressponding to that name
 def get_email(name):
-    # Remove spaces and convert the name to lowercase for the username
     username = ''.join(name.split()).lower()
-
     # Generate a random string of letters and digits to append to the username
     random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=4))
-
     # Generate a random domain for the email
     domains = ['gmail.com', 'yahoo.com', 'outlook.com', 'aol.com', 'hotmail.com']
     domain = random.choice(domains)
-
     # Combine the username, random string, '@', and domain to form the email address
     email = f"{username}{random_string}@{domain}"
-
     return email
 
 def get_number():
     # The first digit of the mobile number is between 7 and 9
     first_digit = random.randint(7, 9)
-    
     # The remaining nine digits can be any number
     remaining_digits = ''.join([str(random.randint(0, 9)) for _ in range(9)])
-    
     # Combine the first digit and remaining digits to form the mobile number
     mobile_number = f"{first_digit}{remaining_digits}"
-
     return mobile_number
 
-
+#genrate fake usn
 def genrate_usn():
     year=["22","21","23"]
     num=random.randint(10,99)
@@ -53,16 +45,15 @@ def genrate_usn():
         return usn+"-T"
     return usn
 
+#creating data frame
 fake_students=pd.DataFrame(columns=["Name","Usn","mail","mobile"])
 
+#populating df
 for i in range(500):
     name = get_name()
     h=[
-             name,
-             genrate_usn(),
-             get_email(name),
-            get_number()
-                ]
+         name,genrate_usn(),get_email(name),get_number()
+     ]
     fake_students.loc[len(fake_students)]=h
   
 
